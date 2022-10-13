f70838f8510d0fd7adf5338489d28990d9a099df
the GOAT <zakiguled@gmail.com>
2022-10-13 22:40:40 +0100

    fixing eightball command magicEightball module merge

--- commands/eightball.js
+++ commands/eightball.js
@@ -15,8 +15,6 @@ module.exports = {
 			return;
 		}
         //Stores response as code block text
-	//Defining module
-module.exports = () => {
     //Stores a random number between 0 and 19
     const randomNumber = Math.floor(Math.random() * 20);
     /*
@@ -24,50 +22,72 @@ module.exports = () => {
     * There are 10 positive responses, 5 indefinite responses and 5 negative responses
     * Fetched responses from Wikipedia
      */
-    const response="```\n";
+    var response="```\n";
     switch (randomNumber) {
         case 0:
             response += "It is certain";
+			break;
         case 1:
             response += "It is decidedly so";
+			break;
         case 2:
             response += "Without a doubt.";
+			break;
         case 3:
             response += "Yes, definitely";
+			break;
         case 4:
             response += "You may rely on it.";
+			break;
         case 5:
             response += "As I see it, yes.";
+			break;
         case 6:
             response += "Most likely.";
+			break;
         case 7:
             response += "Outlook good.";
+			break;
         case 8:
             response += "Yes.";
+			break;
         case 9:
             response += "Signs point to yes.";
+			break;
         case 10:
             response += "Reply hazy, try again.";
+			break;
         case 11:
             response += "Better not tell you now.";
+			break;
         case 12:
             response += "Better not tell you now.";
+			break;
         case 13:
             response += "Cannot predict now.";
+			break;
         case 14:
             response += "Concentrate and ask again.";
+			break;
         case 15:
             response += "Don't count on it.";
+			break;
         case 16:
             response += "My reply is no.";
+			break;
         case 17:
             response += "My sources say no.";
+			break;
         case 18:
             response += "Outlook not so good.";
+			break;
         case 19:
             response += "Very doubtful.";
+			break;
         default:
             response += "ERROR IN MAGIC!";
+			break;
+	}
 	response+="\n```";
         //Creates pre-set embed, adding a title and a field with the pre-defined response
         const embed=embedInit()
