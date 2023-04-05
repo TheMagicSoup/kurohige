2e937d40d20123b1b5b6b2650f9096155e947ef5
the GOAT <zakiguled@gmail.com>
2023-04-05 04:29:32 +0100

    env

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,12 +1,12 @@
 const isSoylisted=require("../commands/modules/isSoylisted.js");
 const getSoymoji=require("../commands/modules/getSoymoji.js");
-
+require("dotenv").config();
 module.exports=(client,message)=>{
 	if(message.author.bot)return;
 	
 	if(isSoylisted(message.author.id))message.react(getSoymoji(client));
 	
-	if(message.content.indexOf(client.config.prefix) !== 0)return;	
+	if(message.content.indexOf(process.env.PREFIX) !== 0)return;	
 	
 	const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
 	const command=args.shift().toLowerCase();
