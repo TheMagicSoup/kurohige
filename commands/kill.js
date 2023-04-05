b108908404c2bf3d5bb373c04be2e6cac7b9c60d
the GOAT <zakiguled@gmail.com>
2023-04-05 07:06:41 +0100

    removing kill

--- commands/kill.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "kill",
-    description: "OWNER-ONLY\nCommand that kills bot process",
-    aliases: ["killemall"],
-    command_usage: "```\nb!kill\n```",
-	run: (client, message, args) => {
-		process.exit();
-	}
-}
